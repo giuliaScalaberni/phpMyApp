4f25d4f5ef84783c2e399c60fb52be864e8601b9
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-20 11:26:08 +0200

    put use

--- putUser.php
+++ putUser.php
@@ -1,23 +1,20 @@
 <?php
 	try{
     $connect=new PDO('mysql:dbname=PhotoEntry;localhost','root', 'root');
-}
+	}
 	catch(PDOException $e){
 		echo 'CONNECTION FAILED: '.$e->getMessage();
 	}
-    $stmt = $connect->prepare("INSERT INTO utente (nome, persistedId, groupId, email) VALUES (:nome, :pers, :idGr, :email)");
-    $stmt->bindValue(':nome', $_POST['nome']);
-    $stmt->bindValue(':pers',$_POST['pers']);
-		$stmt->bindValue(':idGr', $_POST['group']);
-		$stmt->bindValue(':email', $_POST['email']);
-    if ($stmt->execute()) {
-
-
-			echo "ok";
-    }
-    else{
-      echo "id: ".$_POST['pId']."\n";
-        print_r($stmt->errorInfo());
-      }
-
+  $stmt = $connect->prepare("INSERT INTO utente (nome, persistedId, groupId, email) VALUES (:nome, :pers, :idGr, :email)");
+  $stmt->bindValue(':nome', $_POST['nome']);
+  $stmt->bindValue(':pers',$_POST['pers']);
+	$stmt->bindValue(':idGr', $_POST['group']);
+	$stmt->bindValue(':email', $_POST['email']);
+  if ($stmt->execute()) {
+		echo "ok";
+  }
+  else{
+    echo "id: ".$_POST['pId']."\n";
+      print_r($stmt->errorInfo());
+  }
 ?>
